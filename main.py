f7583ff55b0482a33eae463d96dd29abb3d28dcf
EpicEfeathers <76843932+EpicEfeathers@users.noreply.github.com>
2025-04-04 10:47:53 -0400

    Semi - working version
    Ceiling head hit doesn't display properly at high fps

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 
 class FrameControl:
     def __init__(self):
-        self.delta_time = 1
+        self.delta_time = 0.00000000001 # small number so no divide by 0 error when starting program
         self.start_timestamp = time.time()
         self.total_frames = 1
 
@@ -13,38 +13,46 @@ class GameControls:
     def __init__(self):
         self.running = True
         self.debug_screen = True
+        self.debug_screen_height = 8
 
     def toggle_debug_screen(self, screen):
         self.debug_screen = not self.debug_screen
-        screen.clear_buffer(fg=Screen.COLOUR_WHITE, attr=Screen.A_NORMAL, bg=Screen.COLOUR_BLACK, x=0, y=0, w=30, h=7)
+        screen.clear_buffer(fg=Screen.COLOUR_WHITE, attr=Screen.A_NORMAL, bg=Screen.COLOUR_BLACK, x=0, y=0, w=30, h=self.debug_screen_height)
 
     def write_debug_screen(self, screen, player, frame_control):
         if self.debug_screen:
-            screen.clear_buffer(fg=Screen.COLOUR_WHITE, attr=Screen.A_NORMAL, bg=Screen.COLOUR_BLACK, x=0, y=0, w=40, h=7)
-            screen.print_at(f"FPS: {round(1/frame_control.delta_time)}", 0, 0)
+            self.current_line = 0
+
+            # function for printing debug statements easier
+            def print_debug_line(text, colour=Screen.COLOUR_WHITE):
+                screen.print_at(text, 0, self.current_line, colour)
+                self.current_line += 1
+
+            screen.clear_buffer(fg=Screen.COLOUR_WHITE, attr=Screen.A_NORMAL, bg=Screen.COLOUR_BLACK, x=0, y=0, w=40, h=self.debug_screen_height)
+
+            print_debug_line(f"FPS: {round(1/frame_control.delta_time)}")
 
             time_since_start = time.time() - frame_control.start_timestamp
-            screen.print_at(f"Avg FPS: {round(1 / (time_since_start / frame_control.total_frames))}", 0, 1)
+            print_debug_line(f"Avg FPS: {round(1 / (time_since_start / frame_control.total_frames))}")
+            print_debug_line(f"Delta Time: {frame_control.delta_time}")
 
-            screen.print_at(f"Y: {"{:.3f}".format(round(player.y, 3))}", 0, 2, Screen.COLOUR_GREEN)
-            screen.print_at(f"Y velo: {"{:.3f}".format(round(player.y_velo, 3))}", 0, 3, Screen.COLOUR_GREEN)
-            screen.print_at(f"Screen height: {screen.height}", 0, 4, Screen.COLOUR_GREEN)
-            screen.print_at(f"Jumping: {player.jumping}", 0, 5, Screen.COLOUR_GREEN)
+            print_debug_line(f"Y: {"{:.3f}".format(round(player.y, 3))}", Screen.COLOUR_GREEN)
+            print_debug_line(f"Y velo: {"{:.3f}".format(round(player.y_velo, 3))}", Screen.COLOUR_GREEN)
+            print_debug_line(f"Screen height: {screen.height}", Screen.COLOUR_GREEN)
+            print_debug_line(f"Jumping: {player.jumping}", Screen.COLOUR_GREEN)
 
-            #screen.print_at(f"Tile beneath: {screen.get_from(int(player.x), int(player.y+1))}", 0, 5, Screen.COLOUR_BLUE)
-            screen.print_at(f"Tile beneath: {player.get_tile_beneath(screen)}", 0, 6, Screen.COLOUR_BLUE)
+            print_debug_line(f"Tiles: {player.get_tiles_surrounding(screen)}", Screen.COLOUR_BLUE)
+            #screen.print_at(f"Tile beneath: {player.get_tile_beneath(screen)}", 0, 6, Screen.COLOUR_BLUE)
+            #screen.print_at(f"Tile above: {player.get_tile_above(screen)}", 0, 7, Screen.COLOUR_BLUE)
 
 
 
 class Player:
-    def __init__(self, screen_width, screen_height):
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-
+    def __init__(self, screen_width, screen_height): # initializing all player variables
         self.jumping = 0
 
-        self.x = int(self.screen_width / 2)
-        self.y = 10
+        self.x = int(screen_width / 2)
+        self.y = 1
         self.x_velo = 0
         self.y_velo = 0
         #self.movement_speed_x = 0.01
@@ -58,12 +66,25 @@ class Player:
         
         self.tile_beneath = None
 
+    def get_tiles_surrounding(self, screen):
+        surrounding = {
+            "left": screen.get_from(int(self.x - 1), int(self.y)), 
+            "up": screen.get_from(int(self.x), int(self.y - 1)), 
+            "right": screen.get_from(int(self.x + 1), int(self.y)), 
+            "down": screen.get_from(int(self.x), int(self.y + 1))
+        }
+        return surrounding
+
     def get_tile_beneath(self, screen):
         return screen.get_from(int(self.x), int(self.y + 1))
+    
+    def get_tile_above(self, screen):
+        return screen.get_from(int(self.x), int(self.y - 1))
 
 
     def udpate_position(self, screen, DELTA_TIME):
         self.jumping += 1
+
         # old position to check if moved
         self.old_x = self.x
         self.old_y = self.y
@@ -74,11 +95,16 @@ class Player:
         # y axis movement
         self.y_velo += self.gravity * DELTA_TIME
 
+        # check tiles above and beneath to see if valid
         tile_beneath = self.get_tile_beneath(screen)
+        tile_above = self.get_tile_above(screen)
         if tile_beneath is not None and tile_beneath[0] == 9608 and self.jumping > 0: # if tile underneath is block
             #self.y -= 1
             self.y_velo = 0
             self.jumping = 0
+        elif tile_above is not None and tile_above[0] == 9608 and self.y_velo < 0:
+            #da a      self.y += 1
+            self.y_velo = 0
         else:
             self.y += self.y_velo * DELTA_TIME
 
@@ -133,29 +159,35 @@ def demo(screen):
     # non-blocking listener for keyboard inputs
     listener = Listener(on_press=on_press,on_release=on_release)
     listener.start()
-    screen.print_at("█████████████████████", int(screen.width/2 - 8), 11)
+    #screen.print_at("█████████████████████", int(screen.width/2 - 8), random.randint(screen.height - 10, screen.height - 3))
+    for i in range(10):
+        screen.print_at("█", int(screen.width/2 - i), screen.height - i)
 
     for w in range(screen.width): # for some reason, much faster than drawing a line using https://asciimatics.readthedocs.io/en/stable/io.html#drawing-shapes
             screen.print_at("█", w, screen.height-1)
-            
+
     while game_controls.running:
         start = time.time() # start time to calculate delta time
+
         player.udpate_position(screen, frame_control.delta_time)
 
         #screen = draw_player(screen, p)
         screen.print_at(f"o", int(player.x), int(player.y), Screen.COLOUR_WHITE, Screen.A_BOLD)
 
+        for w in range(screen.width): # for some reason, much faster than drawing a line using https://asciimatics.readthedocs.io/en/stable/io.html#drawing-shapes
+            screen.print_at("█", w, screen.height-1)
+
         # instead of clearing entire screen, check if player has moved and clear that position
         if (int(player.old_x) != int(player.x)) or (int(player.old_y) != int(player.y)):
             screen.clear_buffer(fg=Screen.COLOUR_WHITE, attr=Screen.A_NORMAL, bg=Screen.COLOUR_BLACK, x=int(player.old_x), y=int(player.old_y), w=1, h=1)        
 
+        game_controls.write_debug_screen(screen, player, frame_control)
+
         # refresh the display
         screen.refresh()
 
-
-        game_controls.write_debug_screen(screen, player, frame_control)
-
-        #time.sleep(1/60)
+        #if (1/60 - frame_control.delta_time > 0):
+         #   time.sleep(1/60 - frame_control.delta_time)
 
         # calculate delta time
         frame_control.delta_time = time.time() - start
