7cbe37f0bebb8aab71b147124f58172b4a2ef75d
EpicEfeathers <76843932+EpicEfeathers@users.noreply.github.com>
2025-04-09 15:56:18 -0400

    Popup screen is now a class

--- level_editor.py
+++ level_editor.py
@@ -6,27 +6,32 @@ from config import termColors
 
 dimensions = (150, 31)
 
-def show_popup(screen):
-    popup_colour = termColors.popup_gray
-    text_colour = termColors.black
-    global dimensions
-    popup_dimensions = (40, 7)
+class popup_screen:
+    def __init__(self):
+        self.popup_dimensions = (40, 7)
+        self.dimensions = dimensions
+        self.popup_colour = termColors.popup_gray
+        self.text_colour = termColors.black
 
-    # create rectangle
-    start_pos = (int((dimensions[0]-popup_dimensions[0])/2), int((dimensions[1]-popup_dimensions[1])/2))
-    top_right = (start_pos[0] + popup_dimensions[0], start_pos[1])
-    bottom_left = (start_pos[0], start_pos[1] + popup_dimensions[1])
-    bottom_right = (start_pos[0] + popup_dimensions[0], start_pos[1] + popup_dimensions[1])
-    screen.fill_polygon([[(start_pos), (top_right), (bottom_right), (bottom_left)]], colour=popup_colour)
+    def show_popup(self, screen):
+        # create rectangle
+        start_pos = (int((dimensions[0]-self.popup_dimensions[0])/2), int((dimensions[1]-self.popup_dimensions[1])/2))
+        top_right = (start_pos[0] + self.popup_dimensions[0], start_pos[1])
+        bottom_left = (start_pos[0], start_pos[1] + self.popup_dimensions[1])
+        bottom_right = (start_pos[0] + self.popup_dimensions[0], start_pos[1] + self.popup_dimensions[1])
+        screen.fill_polygon([[(start_pos), (top_right), (bottom_right), (bottom_left)]], colour=self.popup_colour)
 
-    text = "Terminal colour (0-255):"
-    screen.print_at(text, int((dimensions[0] - len(text))/2), int(dimensions[1]/2) - 2, text_colour, Screen.A_NORMAL, popup_colour)
+        # add text
+        text = "Terminal colour (0-255):"
+        screen.print_at(text, int((dimensions[0] - len(text))/2), int(dimensions[1]/2) - 2, self.text_colour, Screen.A_NORMAL, self.popup_colour)
 
-    input_dimensions = (4, 1)
-    screen.print_at("    ", int((dimensions[0] - input_dimensions[0])/2), int(dimensions[1]/2), text_colour, Screen.A_NORMAL, termColors.white)
+        # add input box
+        input_dimensions = (4, 1)
+        screen.print_at("    ", int((dimensions[0] - input_dimensions[0])/2), int(dimensions[1]/2), self.text_colour, Screen.A_NORMAL, termColors.white)
 
-    button_text = " Colour diagram (click me!) "
-    screen.print_at(button_text, int((dimensions[0] - len(button_text))/2), int(dimensions[1]/2) + 2, text_colour, Screen.A_NORMAL, termColors.white)
+        # add button
+        button_text = " Colour diagram (click me!) "
+        screen.print_at(button_text, int((dimensions[0] - len(button_text))/2), int(dimensions[1]/2) + 2, self.text_colour, Screen.A_NORMAL, termColors.white)
 
 
 
@@ -62,6 +67,8 @@ def demo(screen):
         screen.print_at("blue", len("Current colour: "), dimensions[1] - 1, Screen.COLOUR_BLUE)
         screen.refresh()
         while not screen.has_resized(): # if screen is correct size
+            popup = popup_screen()
+
             event = screen.get_event()
             if isinstance(event, MouseEvent):
                 if not showing_popup_screen:
@@ -94,8 +101,13 @@ def demo(screen):
                     if showing_popup_screen:
                         showing_popup_screen = False # WORK ON THIS
                     else:
-                        show_popup(screen)
+                        popup.show_popup(screen)
                         showing_popup_screen = True
                     screen.refresh()
+                else:
+                    if showing_popup_screen:
+                        if 48 <= event.key_code <= 57:
+                            screen.print_at(event, 0, 0)
+                            screen.refresh()
 while True:
     Screen.wrapper(demo)
